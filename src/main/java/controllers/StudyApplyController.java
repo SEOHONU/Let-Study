57da14f86c68cb8ad8883f839882a0a0f09158f1
jt5766 <jt5766@naver.com>
2023-05-10 10:41:12 +0900

    Duplicate application not allowed

--- src/main/java/controllers/StudyApplyController.java
+++ src/main/java/controllers/StudyApplyController.java
@@ -43,6 +43,12 @@ public class StudyApplyController extends HttpServlet {
 				int guestcount = dao.updateguestcount(new StudyBoardMembersDTO(null,seq,null));
 				String gresult = g.toJson(guestcount);
 				response.getWriter().append(gresult);
+			}else if(cmd.equals("/multiapply.studyapply")) {
+				int seq = Integer.parseInt(request.getParameter("board_seq"));
+				String id = request.getParameter("id");
+				boolean result = dao.multiapply(new StudyBoardMembersDTO(id, seq, null));
+				String gresult = g.toJson(result);
+				response.getWriter().append(gresult);
 			}
 		}catch(Exception e) {
 			e.printStackTrace();
